9367ccba2c6d5be7f53d4b6543dc8674a1d389af
Bhumi Gupta <bhumigupta175@gmail.com>
2024-03-08 23:45:37 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navigation from './components/Navigation';
 
 function App() {
 
   return (
-   <div> </div>;
+   <div> <Navigation /> </div>
   )
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,14 @@
 const Navigation = () => {
   return (
     <nav>
-<div>
+<div className="logo">
     <img src="/images/logo.png" alt="logo" />
 </div>
-
+<ul>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+</ul>
     </nav>
   )
 }
